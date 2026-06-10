420fce034a4bab4bda26167a207f380302ff3d5d
Rtemise <cheongxin.kang@gmail.com>
2026-06-11 01:12:51 +0800

    Many progress made

--- bot.py
+++ bot.py
@@ -1,35 +1,84 @@
 import os
 import discord
+import werewolf
+
 from discord.ext import commands
 from dotenv import load_dotenv
-GUILD = os.getenv("GUILD_ID")
 
 load_dotenv()
+GUILD = os.getenv("GUILD_ID")
 
-# Enable necessary intents
 intents = discord.Intents.default()
-intents.message_content = True  # Required for reading the !sync text prefix command
+intents.message_content = True 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+game = werewolf.WerewolfGame()
 
-# 1. Define a sample global slash command
-@bot.tree.command(name="ping", description="Responds with a pong!")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("Pong! 🏓")
+def is_mod():
+      async def predicate(ctx):
+            return game.is_mod(ctx.author)
+      return commands.check(predicate)
+
+# ==========================================
+#  DM-ONLY COMMANDS (For Moderator Only)
+# ==========================================
+
+
+@bot.command()
+@commands.dm_only() 
+@is_mod()
+async def cmds(ctx):
+    await ctx.send("The commands available to you: !assign_roles")
+
+
+@bot.command()
+@commands.dm_only()
+@is_mod()
+async def assign_roles(ctx):
+    await ctx.send(game.assign_roles(ctx.author))
+
+
+@bot.command()
+@commands.dm_only()
+@is_mod()
+async def create_roles(ctx, num: int):
+      await ctx.send(game.populate_roles(num))
+
+# ==========================================
+#  SERVER CHANNEL COMMANDS (Anyone Can Use)
+# ==========================================
 
 @bot.tree.command(name="register", description="Register for Werewolf game")
 async def register(interaction: discord.Interaction):
-    await interaction.response.send_message('Registering!', ephemeral=True)
+    await interaction.response.send_message(game.register(interaction.user), ephemeral=True)
 
-# 2. Define the manual sync prefix command
+
+@bot.tree.command(name="players", description="Lists current players for the game")
+async def players(interaction: discord.Interaction):
+      players_message = game.getPlayers()
+      await interaction.response.send_message(players_message, ephemeral=True)
+
+
+@bot.tree.command(name="claim_mod", description="Become the moderator of this werewolf game")
+async def claim_mod(interaction: discord.Interaction):
+      await interaction.response.send_message(game.register_mod(interaction.user))
+      await interaction.user.send("You're the moderator of the werewolf game. DM me privately for mod commands. Use !cmds for list of commands.")
+
+
+@bot.tree.command(name="get_roles", description="Get the list of roles in the game.")
+async def get_roles(interaction: discord.Interaction):
+      await interaction.response.send_message(game.get_roles())
+
+
+# Manual Sync Method
 @bot.command()
 @commands.is_owner()  # Restricts execution to the bot owner only
-async def sync(ctx: commands.Context, guild_id: str = None):
-    if guild_id is None:
-        # Sync globally across all servers (Can take a few minutes to cache)
-        synced = await bot.tree.sync()
-        await ctx.send(f"Successfully synced {len(synced)} global commands.")
-    else:
+async def sync(ctx: commands.Context, guild_id: str = GUILD):
+    # if guild_id is None:
+    #     # Sync globally across all servers (Can take a few minutes to cache)
+    #     synced = await bot.tree.sync()
+    #     await ctx.send(f"Successfully synced {len(synced)} global commands.")
+    # else:
         # Instantly sync to a specific test server for development
         guild_obj = discord.Object(id=int() if not guild_id.isdigit() else int(guild_id))
         bot.tree.copy_global_to(guild=guild_obj)
--- werewolf.py
+++ werewolf.py
@@ -7,21 +7,82 @@ class WerewolfGame:
     roles = dict()
     available_roles = ["werewolf","villager","seer",""]
     players = set()
+    mod = None
+    has_game_started = False
 
-    def __init__(self, num_players):
-        self.populateRoles(num_players)
+    def __init__(self):
+        None
 
-    def populateRoles(self, num_players):
-        self.roles["werewolf"]= 2
+    def populate_roles(self, num_players):
+        n = num_players
+        self.roles["seer"] = 1
+        self.roles["doctor"] = 1
+        self.roles["hunter"] = 1
+        if 6 <= n < 9:
+            self.roles["werewolf"] = 2
+            self.roles["villager"] = n - 5
+            return "Assigned roles."
+        elif 9 <= n < 13:
+            self.roles["werewolf"] = 3
+            self.roles["villager"] = n - 6
+            return "Assigned roles."
+        elif 13 <= n < 17:
+            self.roles["werewolf"] = 4
+            self.roles["villager"] = n - 7
+            return "Assigned roles."
+        elif 17 <= n < 21:
+            self.roles["werewolf"] = 5
+            self.roles["villager"] = n - 8
+            return "Assigned roles."
+        else:
+            self.roles.clear()
+            return "Unable to assign."
 
-    def getRoles(self):
+
+    def get_roles(self):
+        return_string = "The current roles are as follows,"
+        for role in self.roles:
+            return_string += "\n"
+            return_string += f"{role}: {self.roles[role]}"
+        return return_string
+
+    def add_role(self, role_name):
         return 0
-
-    def addRole(self, role_name):
+    
+    def remove_role(self, role_name):
         return 0
     
     def register(self, player_name):
-        self.players.add(player_name)
+        print(self.has_game_started)
+        if not self.has_game_started:
+            self.players.add(player_name)
+            return "You have successfully registered!"
+        else:
+            return "Game has started, please wait until it ends to join in."
+
+    def get_players(self):
+        return_string = "The current list of players are: "
+        for player in self.players:
+            return_string += f"{player.name}, "
+
+        return return_string[:len(return_string)-2]
+
+    def register_mod(self, player):
+        if self.mod is None:
+            self.mod = player
+            return f"{self.mod.name} is now the moderator of your werewolf game."
+        else:
+            return f"There is already a moderator in your werewolf game: {self.mod.name}."
+        
+    def is_mod(self, user):
+        return user.id == self.mod.id
+
+    def assign_roles(self, user):
+        if user.id == self.mod.id:
+            self.has_game_started = True
+            return "Roles have been assigned"
+        else:
+            return "You are not the moderator."
     
     def run(self):
         # this has the logic for running the whole game
